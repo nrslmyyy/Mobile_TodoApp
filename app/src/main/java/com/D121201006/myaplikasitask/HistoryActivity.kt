c5374b52362593d538744e81f09e84f79f273b89
nrslmyyy <nurislamiyah370@gmail.com>
2022-12-14 17:08:36 +0800

    Final Mobile

---
+++ app/src/main/java/com/D121201006/myaplikasitask/HistoryActivity.kt
@@ -0,0 +1,13 @@
+package com.D121201006.myaplikasitask
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.D121201006.myaplikasitask.R
+
+class HistoryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_history)
+    }
+}
